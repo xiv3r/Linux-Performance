a9ecdfae58ff4ebc9703c184ec8f77ca67306114
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 21:40:03 +0800

    Update perf.sh

--- perf.sh
+++ perf.sh
@@ -2,3 +2,6 @@
 
 echo 'echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor' >> /etc/rc.local
 chmod +x /etc/rc.local
+
+echo 'Done Installing into /etc/rc.local...'
+echo 'Require system reboot to apply the settings'

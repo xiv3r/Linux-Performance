ffd15a1056d3e8f6eb243d23d1167842f6bbca0c
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-03 10:00:08 +0800

    Update perf.sh

--- perf.sh
+++ perf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash /etc/rc.local
 
-cd /etc/
 echo 'echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor' >> /etc/rc.local
-chmod +x rc.local
+chmod +x /etc/rc.local

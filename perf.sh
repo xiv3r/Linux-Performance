9c1c492b5524d4c2f18428aaef04a178d1263814
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-05 07:52:25 +0800

    Update perf.sh

--- perf.sh
+++ perf.sh
@@ -1,7 +1,12 @@
 #!/bin/bash /etc/rc.local
 
-echo 'echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor' >> /etc/rc.local
+echo "" > /etc/rc.local
+
+echo "echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" >> /etc/rc.local
+echo "cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq" >> etc/rc.local
+
 chmod +x /etc/rc.local
+echo "exit 0" >> /etc/rc.local
 
 echo 'Done Installing into /etc/rc.local...'
 echo 'Require system reboot to apply the settings'

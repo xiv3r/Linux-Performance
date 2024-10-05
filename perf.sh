4dbbabcd3ec27e3c1f5e5986962d395c2ece808a
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-10-05 08:35:44 +0800

    Update perf.sh

--- perf.sh
+++ perf.sh
@@ -1,10 +1,18 @@
 #!/bin/bash /etc/rc.local
 
-sudo sed -i 's/exit 0//' /etc/rc.local
+# Remove exit 0 from the top
+sed -i 's/exit 0//' /etc/rc.local
 
+# CPU Power Max Performance
 echo "echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" >> /etc/rc.local
+
+# CPU Frequency Max Performance
 echo "cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq" >> /etc/rc.local
+
+# Add exit 0 to the bottom
 echo "exit 0" >> /etc/rc.local
+
+#Make Executable
 chmod +x /etc/rc.local
 
 echo 'Done Installing into /etc/rc.local...'

ecfd26ea7538e228a12afc93522b1344a8174ce5
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-04-16 10:26:03 +0800

    Update perf.sh

--- perf.sh
+++ perf.sh
@@ -8,7 +8,7 @@ cat >>/etc/rc.local << EOF
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
 # CPU Frequency Max Performance
-cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
+cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
 
 # Add exit 0 to the bottom
 exit 0
